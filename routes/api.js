7d8d249d6bdd15d2a6d1bc0727b233cac085e495
MR.D7 <81473463+DAVIDGNZ1@users.noreply.github.com>
2021-05-26 10:55:57 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["MansKey", "MansGans", "MansGanteng", "BeliBang"];
+const listkey = ["DreamKey", "Dreamkey", "davidGanz", "Awok"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
